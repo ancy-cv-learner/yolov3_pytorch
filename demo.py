c410d67d7fef62efe535bb559be1eccd78086e4a
ancy <wu_ac@piec.com.cn>
2019-11-17 18:28:38 +0800

    初始化仓库

--- demo.py
+++ demo.py
@@ -20,11 +20,12 @@ class Cfg:
     img_size = 416
 
 
-def preprocess_img():
-    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
-    img = tf.ToTensor()(Image.open(Cfg.img_path))
+def preprocess_img(img, img_size):
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img = tf.ToTensor()(img)
     img, _ = pad_to_square(img, 0)
-    img = resize(img, Cfg.img_size)
+    img = resize(img, img_size)
+    Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
     input_imgs = img.type(Tensor).unsqueeze(0)
     return input_imgs
 
@@ -32,27 +33,27 @@ def preprocess_img():
 if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    # 数据加载
+    img = cv2.imread(Cfg.img_path)
+    input_imgs = preprocess_img(img, Cfg.img_size)
     # 配置模型
     model = Darknet(Cfg.model_def, img_size=Cfg.img_size).to(device)
     model.load_state_dict(torch.load(Cfg.weights_path))
     model.eval()
-
-    classes = load_classes(Cfg.class_path)  # Extracts class labels from file
-    input_imgs = preprocess_img()
-    # Get detections
+    # 类标签
+    classes = load_classes(Cfg.class_path)
+    # 网络预测
     with torch.no_grad():
         detections = model(input_imgs)
         detections = non_max_suppression(detections, Cfg.conf_thres, Cfg.nms_thres)[0]
 
-    img = cv2.imread(Cfg.img_path)
-    # Draw bounding boxes and labels of detections
+    # 绘制结果
     if detections is not None:
         # Rescale boxes to original image
         detections = rescale_boxes(detections, Cfg.img_size, img.shape[:2])
         unique_labels = detections[:, -1].cpu().unique()
         for x1, y1, x2, y2, conf, cls_conf, cls_pred in detections:
             print("\t+ Label: %s, Conf: %.5f" % (classes[int(cls_pred)], cls_conf.item()))
-
             cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 4)
         cv2.namedWindow('img', 0)
         cv2.imshow('img', img)
